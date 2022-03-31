d934b562ac5d8b80db14e16f588ac26686ac0f10
Aneruth Mohanasundaram <ane1998@gmail.com>
2022-03-31 14:16:43 +0200

    asd

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup,find_packages
 
-with open('README.md', 'r', encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="kolibriData",
-    version = 0.3,
-    author="Aneruth",
-    author_email="ane1998@gmail.com",
-    include_package_data=True,
-    description="Config package for kolibri",
+    name="kolibridata",
+    version="0.1",
+    author="Aneruth,Mohamed Ben Haddou",
+    author_email="ane1998@gmail.com,mbenhaddou@mentis.io",
+    description="A report package for kolibri-ml",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=[],
     url="https://github.com/Aneruth/kolibri-data",
+    project_urls={
+        "Bug Tracker": "https://github.com/Aneruth/kolibri-data/issues",
+    },
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         '': ['*.txt', '*.rst', '*.json', '*.npy', '*.db'],
@@ -22,7 +23,8 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
-        'Development Status :: 4 - Beta',
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
     ],
     python_requires='>=3.7',
 )
